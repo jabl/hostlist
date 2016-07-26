9929dee70a8633e1db9214f990e5d4f2a1a984b8
Janne Blomqvist <blomqvist.janne@gmail.com>
2016-07-26 23:44:23 +0300

    Preliminary implementation of hostlist expansion

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
+// Expand a hostlist to a vector of hostnames
+pub fn expand(hostlist: &str) -> Vec<String> {
+    // Is this a hostlist at all?
+    let baseend = match  hostlist.find('[') {
+        None => return vec![hostlist.to_string()],
+        Some(i) => i,
+    };
+    vec![hostlist[0..baseend].to_string()]
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
     #[test]
     fn it_works() {
+    }
+
+    #[test]
+    fn test_expand() {
+        assert_eq!(expand("foo"), vec!["foo"]);
     }
 }
